2419a7b0496e21686bb9ffe0cf56ba507ca577ea
fred4jupiter <hamsterhase@gmx.de>
2013-12-25 15:09:46 +0100

    removed snake demo

--- src/main/java/de/opitzconsulting/spring4/demo/config/WebConfig.java
+++ src/main/java/de/opitzconsulting/spring4/demo/config/WebConfig.java
@@ -2,7 +2,6 @@
 package de.opitzconsulting.spring4.demo.config;
 
 import de.opitzconsulting.spring4.demo.web.echo.EchoWebSocketHandler;
-import de.opitzconsulting.spring4.demo.web.snake.SnakeWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -12,7 +11,6 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
 
 @Configuration
 @EnableWebMvc
@@ -22,20 +20,12 @@ public class WebConfig extends WebMvcConfigurerAdapter implements WebSocketConfi
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(echoWebSocketHandler(), "/echo");
-        registry.addHandler(snakeWebSocketHandler(), "/snake");
-
         registry.addHandler(echoWebSocketHandler(), "/sockjs/echo").withSockJS();
-        registry.addHandler(snakeWebSocketHandler(), "/sockjs/snake").withSockJS();
     }
 
     @Bean
     public WebSocketHandler echoWebSocketHandler() {
         return new EchoWebSocketHandler();
-    }
-
-    @Bean
-    public WebSocketHandler snakeWebSocketHandler() {
-        return new PerConnectionWebSocketHandler(SnakeWebSocketHandler.class);
     }
 
     // Allow serving HTML files through the default Servlet
