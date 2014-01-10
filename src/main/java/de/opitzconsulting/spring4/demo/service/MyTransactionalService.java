00f7f81d97a9289e96417733169e11595e5695b1
Staehler, Michael <michael.staehler@opitz-consulting.com>
2014-01-10 11:16:04 +0100

    update composed annotation

--- src/main/java/de/opitzconsulting/spring4/demo/service/MyTransactionalService.java
+++ src/main/java/de/opitzconsulting/spring4/demo/service/MyTransactionalService.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public @interface MyTransactionalService {
     String[] value() default "";
 
